adf77000047da2d498f296ecdf59bf48eebe5558
SR2Planner <slimerancher2planner@gmail.com>
2024-06-23 17:26:32 +0200

    add images to free range

--- src/components/planning/FreeRange.js
+++ src/components/planning/FreeRange.js
@@ -1,16 +1,15 @@
 import React from "react";
 import FormControl from "@mui/material/FormControl";
 import Input from "@mui/material/Input";
-import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import { CardActionArea } from "@mui/material";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardMedia from "@mui/material/CardMedia";
 
 const style = {
   position: "absolute",
@@ -37,38 +36,52 @@ const MenuProps = {
 
 const plotTypes = ["Empty", "Slimes", "Hens", "Fruit", "Veggies", "Mixed"];
 const corralSlimeTypes = [
-  "Angler Slime",
-  "Batty Slime",
-  "Boom Slime",
-  "Cotton Slime",
-  "Crystal Slime",
-  "Dervish Slime",
-  "Flutter Slime",
-  "Honey Slime",
-  "Hunter Slime",
-  "Phosphor Slime",
-  "Pink Slime",
-  "Ringtail Slime",
-  "Rock Slime",
-  "Saber Slime",
-  "Tabby Slime",
-  "Yolky Slime",
-  "Nothing",
+  { name: "Angler Slime", image: "slimes/angler.png" },
+  { name: "Batty Slime", image: "slimes/batty.png" },
+  { name: "Boom Slime", image: "slimes/boom.png" },
+  { name: "Cotton Slime", image: "slimes/cotton.png" },
+  { name: "Crystal Slime", image: "slimes/crystal.png" },
+  { name: "Dervish Slime", image: "slimes/dervish.png" },
+  { name: "Flutter Slime", image: "slimes/flutter.png" },
+  { name: "Honey Slime", image: "slimes/honey.png" },
+  { name: "Hunter Slime", image: "slimes/hunter.png" },
+  { name: "Phosphor Slime", image: "slimes/phosphor.png" },
+  { name: "Pink Slime", image: "slimes/pink.png" },
+  { name: "Ringtail Slime", image: "slimes/ringtail.png" },
+  { name: "Rock Slime", image: "slimes/rock.png" },
+  { name: "Saber Slime", image: "slimes/saber.png" },
+  { name: "Tabby Slime", image: "slimes/tabby.png" },
+  { name: "Yolky Slime", image: "slimes/yolky.png" },
+  { name: "None", image: "placeholder.png" },
 ];
 const coopTypes = [
-  "Briar Hen",
-  "Hen Hen",
-  "Painted Hen",
-  "Sea Hen",
-  "Stony Hen",
-  "Thundercluck",
-  "Mixed Hens",
-  "Yolky Slime",
-  "Nothing",
+  { name: "Briar Hen", image: "meats/briarHen.png" },
+  { name: "Hen Hen", image: "meast/henHen.png" },
+  { name: "Painted Hen", image: "meats/paintedHen.png" },
+  { name: "Sea Hen", image: "meast/seaHen.png" },
+  { name: "Stony Hen", image: "meats/stonyHen.png" },
+  { name: "Thundercluck", image: "meats/Thundercluck.png" },
+  { name: "Mixed Meats", image: "meats/mixedMeats.png" },
+  { name: "None", image: "placeholder.png" },
 ];
-const fruitTypes = ["Cuberry", "Mint Mango", "Pogofruit", "Pomegranite", "Prickle Pear", "Nothing"];
-const veggieTypes = ["Carrot", "Heart Bear", "Odd Onion", "Water Lettuce", "Nothing"];
+const fruitTypes = [
+  { name: "Cuberry", image: "placeholder.png" },
+  { name: "Mint Mango", image: "placeholder.png" },
+  { name: "Pogofruit", image: "placeholder.png" },
+  { name: "Pomegranite", image: "placeholder.png" },
+  { name: "Prickle Pear", image: "placeholder.png" },
+  { name: "Mixed Fruits", image: "placeholder.png" },
+  { name: "None", image: "placeholder.png" },
+];
+const veggieTypes = [
+  { name: "Carrot", image: "veggies/carrot.png" },
+  { name: "Heart Beat", image: "veggies/heartBeat.png" },
+  { name: "Odd Onion", image: "veggies/oddOnion.png" },
+  { name: "Water Lettuce", image: "veggies/waterLettuce.png" },
+  { name: "Mixed Veggies", image: "placeholder.png" },
 
+  { name: "None", image: "placeholder.png" },
+];
 export default function FreeRangePlanner({ plotName }) {
   const [open, setOpen] = React.useState(false);
   const [chosenPlotType, setChosenPlotType] = React.useState([]);
@@ -118,26 +131,96 @@ export default function FreeRangePlanner({ plotName }) {
     setChosenContent2(event.target.value);
   };
 
-  let buttonText = "";
-  if (chosenPlotType != "") {
-    if (chosenContent1 != "" && chosenContent2 != "") {
-      buttonText = `${chosenContent1} and ${chosenContent2}`;
-    } else if (chosenContent1 == "") {
-      buttonText = `${chosenContent2}`;
-    } else if (chosenContent2 == "") {
-      buttonText = `${chosenContent1}`;
+  let plotText = "";
+  let plotContentImages = <div />;
+
+  if (chosenPlotType != "" && chosenPlotType != "Empty") {
+    plotText = ` - ${chosenPlotType}`;
+  } else {
+    plotText = "";
+  }
+
+  if (chosenPlotType != "" && chosenPlotType != "Empty") {
+    if (
+      chosenContent1.name !== undefined &&
+      chosenContent2.name !== undefined &&
+      chosenContent1.name != "" &&
+      chosenContent2.name != "" &&
+      chosenContent1.name != "None" &&
+      chosenContent2.name != "None"
+    ) {
+      plotContentImages = (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <CardMedia
+            component="img"
+            alt={chosenContent1.name}
+            height="40"
+            width="40"
+            sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
+            image={require(`../../../public/images/${chosenContent1.image}`)}
+          />
+          <CardMedia
+            component="img"
+            alt={chosenContent2.name}
+            height="40"
+            width="40"
+            sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
+            image={require(`../../../public/images/${chosenContent2.image}`)}
+          />
+        </Box>
+      );
+    } else if (
+      chosenContent1.name !== undefined &&
+      chosenContent1.name != "" &&
+      chosenContent1.name != "None"
+    ) {
+      plotContentImages = (
+        <CardMedia
+          component="img"
+          alt={chosenContent1.name}
+          height="40"
+          width="40"
+          sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
+          image={require(`../../../public/images/${chosenContent1.image}`)}
+        />
+      );
+    } else if (
+      chosenContent2.name !== undefined &&
+      chosenContent2.name != "" &&
+      chosenContent2.name != "None"
+    ) {
+      plotContentImages = (
+        <CardMedia
+          component="img"
+          alt={chosenContent2.name}
+          height="40"
+          width="40"
+          sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
+          image={require(`../../../public/images/${chosenContent2.image}`)}
+        />
+      );
+    } else {
+      plotContentImages = <div />;
     }
+  } else {
+    plotContentImages = <div />;
   }
 
   return (
     <div>
       <Card>
-        <CardActionArea onClick={handleOpen}>
+      <CardActionArea onClick={handleOpen}>
           <CardContent>
             <Typography variant="body2" fontWeight={600}>
               {plotName}
+              {plotText}
             </Typography>
-            <Typography variant="body2">{buttonText}</Typography>
+            {plotContentImages}
           </CardContent>
         </CardActionArea>
       </Card>
@@ -179,9 +262,9 @@ export default function FreeRangePlanner({ plotName }) {
               <MenuItem value="">
                 <em>Plot Content 1</em>
               </MenuItem>
-              {contentOptions.map((name) => (
-                <MenuItem key={name} value={name}>
-                  {name}
+              {contentOptions.map((content) => (
+                <MenuItem key={content} value={content}>
+                  {content.name}
                 </MenuItem>
               ))}
             </Select>
@@ -197,9 +280,9 @@ export default function FreeRangePlanner({ plotName }) {
               <MenuItem value="">
                 <em>Plot Content 2</em>
               </MenuItem>
-              {contentOptions.map((name) => (
-                <MenuItem key={name} value={name}>
-                  {name}
+              {contentOptions.map((content) => (
+                <MenuItem key={content} value={content}>
+                  {content.name}
                 </MenuItem>
               ))}
             </Select>
