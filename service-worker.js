fadef0c349aea4a80462ad5253467e013c4b7cfa
SR2Planner <slimerancher2planner@gmail.com>
2024-07-01 17:51:59 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SR2Planner/precache-manifest.4f1192e4e0a4a555f4ebfe54052341d6.js"
+  "/SR2Planner/precache-manifest.eeef8ccc23a213a138dce110071ea982.js"
 );
 
 self.addEventListener('message', (event) => {
